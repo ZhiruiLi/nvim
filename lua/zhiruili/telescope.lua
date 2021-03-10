31c4ca84a1b7a65eca9181004b1922ce5e9d9b53
zhiruili <zr.public@outlook.com>
2021-03-10 21:48:30 +0800

    remove some telescope func

--- lua/zhiruili/telescope.lua
+++ lua/zhiruili/telescope.lua
@@ -35,22 +35,5 @@ M.search_dotfiles = function()
   })
 end
 
-M.search_dirfiles = function()
-  require("telescope.builtin").find_files({
-    prompt_title = "< Current Dir >",
-    cwd = vim.fn.expand("%:p:h"),
-  })
-end
-
-M.git_branches = function()
-  require("telescope.builtin").git_branches({
-    attach_mappings = function(prompt_bufnr, map)
-      map('i', '<c-d>', actions.git_delete_branch)
-      map('n', '<c-d>', actions.git_delete_branch)
-      return true
-    end
-  })
-end
-
 return M
 
